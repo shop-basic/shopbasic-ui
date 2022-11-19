539ddb282e426cbeeb1f12bad557b77998a385fb
Jagmeet Singh <jagmeet.singh03@sap.com>
2022-11-20 03:02:35 +0530

    ui enhancements

--- src/app/views/product/product-detail/product-detail.component.ts
+++ src/app/views/product/product-detail/product-detail.component.ts
@@ -8,7 +8,7 @@ import { Iproduct } from 'src/app/models/iproduct';
 })
 export class ProductDetailComponent implements OnInit {
 
-  public products: Iproduct[] = [
+  public product: Iproduct = 
     {
       name :'Product 1',
       price : 100,
@@ -19,41 +19,54 @@ export class ProductDetailComponent implements OnInit {
       manufacturer: 'Manufacturer 1',
       avatar : './assets/img/avatars/1.jpg',
       productId : 'P1',
-    },
-    {
-      name :'Product 1',
-      price : 100,
-      packagingQuantity : 1,
-      availableQuantity : 100,
-      unit :'lt.',
-      activeFlag: true,
-      manufacturer: 'Manufacturer 1',
-      avatar : './assets/img/avatars/1.jpg',
-      productId : 'P1',
-    },
-    {
-      name :'Product 1',
-      price : 100,
-      packagingQuantity : 1,
-      availableQuantity : 100,
-      unit :'lt.',
-      activeFlag: true,
-      manufacturer: 'Manufacturer 1',
-      avatar : './assets/img/avatars/1.jpg',
-      productId : 'P1',
-    },
-    {
-      name :'Product 1',
-      price : 100,
-      packagingQuantity : 1,
-      availableQuantity : 100,
-      unit :'lt.',
-      activeFlag: true,
-      manufacturer: 'Manufacturer 1',
-      avatar : './assets/img/avatars/1.jpg',
-      productId : 'P1',
-    }
-  ];
+    };
+
+    public products: Iproduct[] = [
+      {
+        name :'Product 1',
+        price : 100,
+        packagingQuantity : 500,
+        availableQuantity : 50,
+        unit :'ml',
+        activeFlag: true,
+        manufacturer: 'Manufacturer 1',
+        avatar : './assets/img/avatars/1.jpg',
+        productId : 'P1',
+      },
+      {
+        name :'Product 1',
+        price : 100,
+        packagingQuantity : 1,
+        availableQuantity : 100,
+        unit :'lt',
+        activeFlag: true,
+        manufacturer: 'Manufacturer 1',
+        avatar : './assets/img/avatars/1.jpg',
+        productId : 'P1',
+      },
+      {
+        name :'Product 1',
+        price : 100,
+        packagingQuantity : 2,
+        availableQuantity : 200,
+        unit :'lt',
+        activeFlag: true,
+        manufacturer: 'Manufacturer 1',
+        avatar : './assets/img/avatars/1.jpg',
+        productId : 'P1',
+      },
+      {
+        name :'Product 1',
+        price : 100,
+        packagingQuantity : 10,
+        availableQuantity : 1000,
+        unit :'lt',
+        activeFlag: true,
+        manufacturer: 'Manufacturer 1',
+        avatar : './assets/img/avatars/1.jpg',
+        productId : 'P1',
+      }
+    ]
   
   constructor() { }
 
